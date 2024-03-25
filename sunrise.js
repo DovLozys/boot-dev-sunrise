d5b94b2d5b4131e1989cdeafa8cb080560940cb0
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2024-03-25 11:53:50 +0000

    `filter: invert` all the things

--- sunrise.js
+++ sunrise.js
@@ -1,2 +1,8 @@
 document.body.style.border = "5px solid red";
+document.body.style.filter = "invert(100%) hue-rotate(180deg)";
 
+let images = document.getElementsByTagName("img");
+
+for (i = 0; i < images.length; i++) {
+  images[i].style.filter = "invert(1) hue-rotate(180deg)";
+}
